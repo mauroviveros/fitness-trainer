f40361511b097a79157164057c5f39f8d5c87e77
Mauro Daniel Viveros <mviveros@teknosgroup.com>
2023-02-24 21:25:39 -0300

    feat(auth): Add error messages

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth.routing';
 import { MaterialModule } from '../material.module';
+
 import { LoginComponent } from './pages/login/login.component';
 
-
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
+  declarations: [LoginComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     AuthRoutingModule,
     MaterialModule
   ]
 })
-export class AuthModule { }
+export class AuthModule {}
--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'auth-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  public show_password = false;
 
+  public form: FormGroup = this._formBuilder.group({
+    user: [null, [Validators.required]],
+    password: [null, [Validators.required]]
+  });
+
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _snackBar: MatSnackBar
+  ) {}
+
+  public login(): void {
+    this.form.markAllAsTouched();
+    if (this.form.invalid) return;
+
+    this._snackBar.open("❌ Usuario o contraseña incorrecta");
+  }
+
+  public isInvalid(campo: string): boolean | null{
+    return this.form.controls[campo].errors && this.form.controls[campo].touched;
+  }
 }
