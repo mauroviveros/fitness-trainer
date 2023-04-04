27b14df7fa4c41f10161a00bfc93c525d8f797cd
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-04-03 23:50:32 -0300

    build: :heavy_minus_sign: remove dotenv

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  FIREBASE: {
+    APP_ID              : "1:752639024627:web:a5f80ace707098191c0686",
+    API_KEY             : "AIzaSyD8Cqe4iAUwHKlwYnCgQMbuiCYWFsN3T5c",
+    AUTH_DOMAIN         : "fitness-trainer-dev-a0ab6.firebaseapp.com",
+    MEASUREMENT_ID      : "G-93C0PQ04F2",
+    MESSAGING_SENDER_ID : "752639024627",
+    PROJECT_ID          : "fitness-trainer-dev-a0ab6",
+    STORAGE_BUCKET      : "fitness-trainer-dev-a0ab6.appspot.com"
+  }
+};
