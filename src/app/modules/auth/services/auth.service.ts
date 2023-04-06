c7695d73ef3abd588d407baf39d733cc820a5b82
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-04-06 18:32:23 -0300

    fix(auth): :bulb: disable persistence

--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { Auth, signInWithEmailAndPassword } from "@angular/fire/auth";
+import { Auth, setPersistence, signInWithEmailAndPassword } from "@angular/fire/auth";
 import { from } from "rxjs";
 
 @Injectable({
@@ -9,7 +9,9 @@ export class AuthService {
 
   constructor(
     private auth: Auth
-  ){}
+  ){
+    // setPersistence(this.auth, { type: "LOCAL" });
+  }
 
   public login(email: string, password: string){
     return from(signInWithEmailAndPassword(this.auth, email, password));
