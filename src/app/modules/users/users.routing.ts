fb50ae24491f1e9266ce12338f6b33138dc3a890
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-04-06 12:58:20 -0300

    feat(users): :sparkles: add routing

---
+++ src/app/modules/users/users.routing.ts
@@ -0,0 +1,25 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { DetailComponent } from "./pages/detail/detail.component";
+import { ListComponent } from "./pages/list/list.component";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: ListComponent
+  },
+  {
+    path: ":_id",
+    component: DetailComponent
+  },
+  {
+    path: "**",
+    redirectTo: ""
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class UsersRoutingModule { }
