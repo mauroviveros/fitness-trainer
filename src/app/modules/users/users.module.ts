fdc9131166037f48dcb0e38196fe102f3ae1507e
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-04-10 17:53:31 -0300

    feat(users): :fire: Remove BottomSheet

--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -8,15 +8,13 @@ import { SharedModule } from "../../shared/shared.module";
 import { ListComponent } from "./pages/list/list.component";
 import { DetailComponent } from "./pages/detail/detail.component";
 import { NewUserComponent } from "./components/new-user/new-user.component";
-import { BottomSheetComponent } from "./components/bottom-sheet/bottom-sheet.component";
 
 
 @NgModule({
   declarations: [
     ListComponent,
     DetailComponent,
-    NewUserComponent,
-    BottomSheetComponent
+    NewUserComponent
   ],
   imports: [
     CommonModule,
