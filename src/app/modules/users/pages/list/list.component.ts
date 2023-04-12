e804685d0cade61343daaee1f97c19450cf3c26e
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-04-11 22:11:14 -0300

    feat(users): :sparkles: add list

--- src/app/modules/users/pages/list/list.component.ts
+++ src/app/modules/users/pages/list/list.component.ts
@@ -2,6 +2,8 @@ import { Component } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
 
 import { NewUserComponent } from "../../components/new-user/new-user.component";
+import { UsersService } from "../../services/users.service";
+import { Role, UserDocument } from "../../interfaces/users";
 
 @Component({
   selector: "app-list",
@@ -9,13 +11,26 @@ import { NewUserComponent } from "../../components/new-user/new-user.component";
   styleUrls: ["./list.component.scss"]
 })
 export class ListComponent {
+  public users: UserDocument[] = [];
 
   constructor(
     private _dialog: MatDialog,
-  ){}
+    private usersService: UsersService
+  ){
+    this.usersService.getUsers().subscribe(users => {
+      this.users = users;
+    });
+  }
+
+  public getIconByRol(role: Role){
+    switch(role){
+    case "USER": return "person";
+    case "ADMIN": return "supervised_user_circle";
+    case "OWNER": return "engineering";
+    }
+  }
 
   public addUser(){
     this._dialog.open(NewUserComponent);
-
   }
 }
