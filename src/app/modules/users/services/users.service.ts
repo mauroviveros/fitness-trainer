f0681b5be9d0d89678650ad275b926f4faf4dfce
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-04-13 17:39:41 -0300

    fix(users): :alien: add BehaviorSubject on service

--- src/app/modules/users/services/users.service.ts
+++ src/app/modules/users/services/users.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from "@angular/core";
 import { Auth, createUserWithEmailAndPassword, fetchSignInMethodsForEmail } from "@angular/fire/auth";
-import { Firestore, collection, setDoc, doc, collectionData, updateDoc, docSnapshots } from "@angular/fire/firestore";
+import { Firestore, collection, setDoc, doc, collectionData, updateDoc, docSnapshots, DocumentData, DocumentSnapshot } from "@angular/fire/firestore";
 
 import { UserDocument, UserDocumentOutput } from "../interfaces/users";
-import { from, map, switchMap } from "rxjs";
+import { from, map, switchMap, BehaviorSubject, filter } from "rxjs";
 import { AuthService } from "../../auth/services/auth.service";
 
 @Injectable({
@@ -11,12 +11,30 @@ import { AuthService } from "../../auth/services/auth.service";
 })
 export class UsersService {
   private usersCollection = collection(this.firestore, "users");
+  public _user = new BehaviorSubject<UserDocument>({} as UserDocument);
+  public get user(){ return this._user.pipe(filter((user) => !!user._id)); }
+
+  private parseUserDocument(document: DocumentSnapshot<DocumentData>){
+    if(!document.exists()) throw new Error();
+    const user = document.data();
+    user["_id"] = document.id;
+    user["dateCreated"] = user["dateCreated"].toDate();
+    user["dateLastEmailVerification"] = user["dateLastEmailVerification"] ? user["dateLastEmailVerification"].toDate() : undefined;
+    return user as UserDocument;
+  }
 
   constructor(
     private auth: Auth,
     private authService: AuthService,
     private firestore: Firestore
-  ){}
+  ){
+    this.authService.user.pipe(
+      switchMap( user => docSnapshots(doc(this.usersCollection, user.uid))),
+      map(this.parseUserDocument)
+    ).subscribe(user => {
+      this._user.next(user);
+    });
+  }
 
   public createUser(email: string, fields: UserDocument){
     const promise = async () => {
@@ -37,26 +55,10 @@ export class UsersService {
     return collectionData(this.usersCollection, { idField: "_id" });
   }
 
-  public getUser(_id?: string){
-    const snapshot = () => docSnapshots(doc(this.usersCollection, _id)).pipe(
-      map(documentData => {
-        if(!documentData.exists()) throw new Error();
-        const user = documentData.data();
-        user["_id"] = documentData.id;
-        user["dateCreated"] = user["dateCreated"].toDate();
-        user["dateLastEmailVerification"] = user["dateLastEmailVerification"] ? user["dateLastEmailVerification"].toDate() : undefined;
-        return user as UserDocument;
-      }),
+  public getUser(_id: string){
+    return docSnapshots(doc(this.usersCollection, _id)).pipe(
+      map(this.parseUserDocument),
     );
-
-    if(!_id) return this.authService.getUser().pipe(
-      switchMap(user => {
-        _id = user.uid;
-        return snapshot();
-      })
-    ); else{
-      return snapshot();
-    }
   }
 
 }
