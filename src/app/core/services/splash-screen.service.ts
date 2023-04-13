69e68a079c15dad84aeb9286794f38e52fd24700
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-04-13 17:27:23 -0300

    feat(core): :sparkles: add SplashScreen with service

---
+++ src/app/core/services/splash-screen.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, combineLatest } from "rxjs";
+
+import { AuthService } from "src/app/modules/auth/services/auth.service";
+import { UsersService } from "src/app/modules/users/services/users.service";
+
+@Injectable({
+  providedIn: "root"
+})
+export class SplashScreenService {
+  public isLoading = new BehaviorSubject<boolean>(true);
+
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService,
+  ){
+    combineLatest([this.authService.user, this.usersService.user]).subscribe(() => {
+      this.setLoading(false);
+    });
+  }
+
+  public setLoading(value: boolean){ this.isLoading.next(value); }
+}
