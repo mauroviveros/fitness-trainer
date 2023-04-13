96d4b84f30f3484882bd9ea82ee4335317ec80a1
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-04-13 18:13:20 -0300

    fix(core): :bug: fix hide splashScreen if authState null

--- src/app/core/services/splash-screen.service.ts
+++ src/app/core/services/splash-screen.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
-import { BehaviorSubject, combineLatest } from "rxjs";
+import { Auth, authState } from "@angular/fire/auth";
+import { BehaviorSubject, combineLatest, switchMap, filter, tap } from "rxjs";
 
 import { AuthService } from "src/app/modules/auth/services/auth.service";
 import { UsersService } from "src/app/modules/users/services/users.service";
@@ -11,12 +12,15 @@ export class SplashScreenService {
   public isLoading = new BehaviorSubject<boolean>(true);
 
   constructor(
+    private auth: Auth,
     private authService: AuthService,
     private usersService: UsersService,
   ){
-    combineLatest([this.authService.user, this.usersService.user]).subscribe(() => {
-      this.setLoading(false);
-    });
+    authState(this.auth).pipe(
+      tap(user => { if(!user) this.setLoading(false);}),
+      filter(user => !!user),
+      switchMap(() => combineLatest([this.authService.user, this.usersService.user]))
+    ).subscribe(() => { this.setLoading(false); });
   }
 
   public setLoading(value: boolean){ this.isLoading.next(value); }
