2410b33cf609c0cbeb2da0d3fffcf4b9a9a554a5
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-04-13 14:08:45 -0300

    feat(auth): :sparkles: can sendEmail each 5minutes

--- src/app/modules/auth/pages/unverified/unverified.component.ts
+++ src/app/modules/auth/pages/unverified/unverified.component.ts
@@ -2,6 +2,8 @@ import { Component } from "@angular/core";
 import { Router } from "@angular/router";
 import { User } from "@angular/fire/auth";
 
+import * as moment from "moment";
+
 import { AuthService } from "../../services/auth.service";
 import { UserDocument } from "src/app/modules/users/interfaces/users";
 import { UsersService } from "src/app/modules/users/services/users.service";
@@ -11,20 +13,28 @@ import { UsersService } from "src/app/modules/users/services/users.service";
   templateUrl: "./unverified.component.html",
   styleUrls: ["./unverified.component.scss"]
 })
-export class UnverifiedComponent {
+export class UnverifiedComponent{
   public authUser: User = {} as User;
   public user: UserDocument = {} as UserDocument;
+
+  public get canSendEmail(){
+    if(!this.user.dateLastEmailVerification) return true;
+    return moment().diff(moment(this.user.dateLastEmailVerification), "minutes") >= 5;
+  }
 
   constructor(
     private auth: AuthService,
     private router: Router,
     private userService: UsersService
   ){
-    this.userService.getUser().subscribe(user => { this.user = user; });
+    this.userService.getUser().subscribe(user => {
+      this.user = user;
+    });
   }
 
-  public sendVerification = this.auth.sendEmailVerification;
-
+  public sendVerification(){
+    this.auth.sendEmailVerification().then(() => this.userService.updateUser(this.user._id, { dateLastEmailVerification: new Date() }));
+  }
 
   public logout(){
     this.auth.logout().then(() => this.router.navigate(["/login"]));
