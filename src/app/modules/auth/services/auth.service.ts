0626e34de1319f786d3a0910d0d21f641c2dfb16
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-04-15 17:15:46 -0300

    fix(auth): :wastebasket: cleaned authService

--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -1,44 +1,24 @@
 import { Injectable } from "@angular/core";
-import { Auth, signInWithEmailAndPassword, signOut, authState, sendEmailVerification, User } from "@angular/fire/auth";
-import { BehaviorSubject, lastValueFrom, from, map, switchMap, filter } from "rxjs";
+import { Auth, signInWithEmailAndPassword, signOut, User, user, createUserWithEmailAndPassword } from "@angular/fire/auth";
+import { BehaviorSubject, filter } from "rxjs";
 
 @Injectable({
   providedIn: "root"
 })
 export class AuthService {
-  private _user = new BehaviorSubject<User>({} as User);
-  public get user(){ return this._user.pipe(filter((user) => !!user.uid)); }
+  private _user = new BehaviorSubject<User | null>(null);
+  get user(){ return this._user.pipe(filter(user => !!user)); }
 
   constructor(
     private auth: Auth,
   ){
-    authState(this.auth).pipe(
-      filter(user => !!user),
-      map(user => user as User)
-    ).subscribe(user => {
+    user(this.auth).subscribe(user => {
       this._user.next(user);
     });
-    // setPersistence(this.auth, { type: "LOCAL" });
+    //// setPersistence(this.auth, { type: "LOCAL" });
   }
 
-  public getUser(){
-    return authState(this.auth).pipe(
-      filter(user => !!user),
-      map(user => { return user as User; })
-    );
-  }
-
-  public login(email: string, password: string){
-    return from(signInWithEmailAndPassword(this.auth, email, password));
-  }
-
-  public sendEmailVerification(){
-    return lastValueFrom(this.user.pipe(
-      switchMap(user => sendEmailVerification(user))
-    ));
-  }
-
-  public logout(){
-    return signOut(this.auth);
-  }
+  login(email: string, password: string){ return signInWithEmailAndPassword(this.auth, email, password); }
+  register(email: string, password: string){ return createUserWithEmailAndPassword(this.auth, email, password); }
+  logout(){ return signOut(this.auth); }
 }
