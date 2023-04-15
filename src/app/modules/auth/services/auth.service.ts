d855a3dfe521190a0080eb4c1f05f9c9babeb932
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-04-15 17:30:22 -0300

    fix(auth): :monocle_face: add 2 getters from user

--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from "@angular/core";
 import { Auth, signInWithEmailAndPassword, signOut, User, user, createUserWithEmailAndPassword } from "@angular/fire/auth";
-import { BehaviorSubject, filter } from "rxjs";
+import { BehaviorSubject, filter, map } from "rxjs";
 
 @Injectable({
   providedIn: "root"
 })
 export class AuthService {
   private _user = new BehaviorSubject<User | null>(null);
-  get user(){ return this._user.pipe(filter(user => !!user)); }
+  get user(){ return this._user.asObservable(); }
+  get USER(){
+    return this._user.pipe(
+      filter(user => !!user),
+      map(user => user as User)
+    );
+  }
 
   constructor(
     private auth: Auth,
