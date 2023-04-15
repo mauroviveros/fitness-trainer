5bc3ecdc589b50bea63e721a6127e8b141067337
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-04-15 17:53:26 -0300

    fix(core): :wastebasket: clean SplashScreenService

--- src/app/core/services/splash-screen.service.ts
+++ src/app/core/services/splash-screen.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from "@angular/core";
-import { Auth, authState } from "@angular/fire/auth";
-import { BehaviorSubject, combineLatest, switchMap, filter, tap } from "rxjs";
+import { BehaviorSubject, combineLatest } from "rxjs";
 
 import { AuthService } from "src/app/modules/auth/services/auth.service";
 import { UsersService } from "src/app/modules/users/services/users.service";
@@ -12,15 +11,12 @@ export class SplashScreenService {
   public isLoading = new BehaviorSubject<boolean>(true);
 
   constructor(
-    private auth: Auth,
     private authService: AuthService,
     private usersService: UsersService,
   ){
-    authState(this.auth).pipe(
-      tap(user => { if(!user) this.setLoading(false);}),
-      filter(user => !!user),
-      switchMap(() => combineLatest([this.authService.user, this.usersService.user]))
-    ).subscribe(() => { this.setLoading(false); });
+    combineLatest([this.authService.user]).subscribe(() => {
+      this.setLoading(false);
+    });
   }
 
   public setLoading(value: boolean){ this.isLoading.next(value); }
