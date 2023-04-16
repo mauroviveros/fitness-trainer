d64e5fd3c7d0a420e46d2aa4d5423b4873927a4e
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-04-16 15:34:09 -0300

    feat(auth): :passport_control: add unauthorized dialog component

---
+++ src/app/modules/auth/components/unauthorized/unauthorized.component.ts
@@ -0,0 +1,25 @@
+import { Component, Inject } from "@angular/core";
+import { User } from "@angular/fire/auth";
+import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { AuthService } from "../../services/auth.service";
+
+interface DialogData{
+  user: User
+}
+
+@Component({
+  selector: "app-unauthorized",
+  templateUrl: "./unauthorized.component.html",
+  styleUrls: ["./unauthorized.component.scss"]
+})
+export class UnauthorizedComponent {
+  constructor(
+    private auth: AuthService,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ){}
+
+
+  sendEmailVerification(){
+    this.auth.sendEmailVerification(this.data.user);
+  }
+}
