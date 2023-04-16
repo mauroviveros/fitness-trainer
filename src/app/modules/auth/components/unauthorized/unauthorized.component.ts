6b0b72384a387b8176c2f99f5a26cbb493f901a0
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-04-16 18:09:20 -0300

    feat(auth): :sparkles: complete unauthorized component

--- src/app/modules/auth/components/unauthorized/unauthorized.component.ts
+++ src/app/modules/auth/components/unauthorized/unauthorized.component.ts
@@ -1,7 +1,8 @@
 import { Component, Inject } from "@angular/core";
 import { User } from "@angular/fire/auth";
-import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
 import { AuthService } from "../../services/auth.service";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 interface DialogData{
   user: User
@@ -14,12 +15,16 @@ interface DialogData{
 })
 export class UnauthorizedComponent {
   constructor(
+    private dialog: MatDialogRef<UnauthorizedComponent>,
+    private snackBar: MatSnackBar,
     private auth: AuthService,
     @Inject(MAT_DIALOG_DATA) public data: DialogData
   ){}
 
 
   sendEmailVerification(){
-    this.auth.sendEmailVerification(this.data.user);
+    this.auth.sendEmailVerification(this.data.user)
+      .finally(() => this.dialog.close())
+      .then(() => this.snackBar.open("✅ Te enviamos un correo para validar tu email", undefined));
   }
 }
