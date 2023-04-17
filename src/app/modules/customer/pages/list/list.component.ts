ad85341814dfd430220e598e15b58c0bfb3afac7
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-04-17 02:04:17 -0300

    feat(customer): :sparkles: add service list customers

--- src/app/modules/customer/pages/list/list.component.ts
+++ src/app/modules/customer/pages/list/list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { CustomerService } from "../../services/customer.service";
+import { UserDocument } from "src/app/modules/auth/interfaces/user";
 
 @Component({
   selector: "app-list",
@@ -6,5 +8,14 @@ import { Component } from "@angular/core";
   styleUrls: ["./list.component.scss"]
 })
 export class ListComponent {
+  customers: UserDocument[] = [];
+
+  constructor(
+    private customerService: CustomerService
+  ){
+    this.customerService.customers.subscribe(customers => {
+      this.customers = customers;
+    });
+  }
 
 }
--- src/app/modules/customer/services/customer.service.ts
+++ src/app/modules/customer/services/customer.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from "@angular/core";
+import { Firestore, collection, collectionData, query, where } from "@angular/fire/firestore";
+import { BehaviorSubject } from "rxjs";
+import { UserDocument } from "../../auth/interfaces/user";
 
 @Injectable({
   providedIn: "root"
 })
 export class CustomerService {
+  private customerCollection = collection(this.firestore, "users");
+  private _customers = new BehaviorSubject<UserDocument[]>([]);
+  get customers(){ return this._customers.asObservable(); }
 
-  // constructor(){}
+  constructor(
+    private firestore: Firestore
+  ){
+    collectionData(query(this.customerCollection, where("admin", "!=", true)), { idField: "_id" }).subscribe(customers => {
+      console.log(customers);
+      this._customers.next(customers as UserDocument[]);
+    });
+  }
 }
