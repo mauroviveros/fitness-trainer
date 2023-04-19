b0c9834ea6761d8f3b80bc6cfd69a8af24653fce
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-04-19 18:30:41 -0300

    chore(auth): :coffin: remove snackBar

--- src/app/modules/auth/components/unauthorized/unauthorized.component.ts
+++ src/app/modules/auth/components/unauthorized/unauthorized.component.ts
@@ -1,8 +1,8 @@
 import { Component, Inject } from "@angular/core";
 import { User } from "@angular/fire/auth";
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+
 import { AuthService } from "../../services/auth.service";
-import { MatSnackBar } from "@angular/material/snack-bar";
 
 interface DialogData{
   user: User
@@ -16,15 +16,12 @@ interface DialogData{
 export class UnauthorizedComponent {
   constructor(
     private dialog: MatDialogRef<UnauthorizedComponent>,
-    private snackBar: MatSnackBar,
     private auth: AuthService,
     @Inject(MAT_DIALOG_DATA) public data: DialogData
   ){}
 
 
   sendEmailVerification(){
-    this.auth.sendEmailVerification(this.data.user)
-      .finally(() => this.dialog.close())
-      .then(() => this.snackBar.open("✅ Te enviamos un correo para validar tu email", undefined));
+    this.auth.sendEmailVerification(this.data.user).finally(() => this.dialog.close());
   }
 }
