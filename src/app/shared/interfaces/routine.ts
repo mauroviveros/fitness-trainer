a6ec102984a86deef5d4f5514c85987644b7cdc2
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-04-29 15:55:43 -0300

    feat(routine): :label: add interfaces for routines

--- src/app/shared/interfaces/routine.ts
+++ src/app/shared/interfaces/routine.ts
@@ -1,4 +1,30 @@
 import { DocumentData, DocumentReference } from "@angular/fire/firestore";
+import { UserDocument } from "src/app/modules/auth/interfaces/user";
+import { Exercise } from "./exercises";
+
+export interface Routine{
+    _id         : string
+    admin       : UserDocument
+    customer    : UserDocument
+    dateIN      : Date
+    dateOUT     : Date
+    days        : number[]
+    level       : string
+    kal         : number
+    objective   : string
+    exercises?  : RoutineExercise[]
+}
+
+export interface RoutineExerciseWeigh{
+    meta: number
+    real: number
+}
+export interface RoutineExercise extends Exercise{
+    day: number
+    reps: number
+    series: number
+    weighs: RoutineExerciseWeigh[]
+}
 
 export interface RoutineOUT{
     admin?      : string | DocumentReference<DocumentData>
