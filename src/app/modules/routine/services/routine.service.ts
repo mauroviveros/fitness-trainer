a831c9f4decbebc4b309eeccc0ad95f622da116a
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-04-29 15:56:40 -0300

    feat(routine): :alien: add getter and updater routine service

--- src/app/modules/routine/services/routine.service.ts
+++ src/app/modules/routine/services/routine.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from "@angular/core";
 import { MatSnackBar } from "@angular/material/snack-bar";
-import { Firestore, collection, doc, setDoc } from "@angular/fire/firestore";
+import { Firestore, collection, doc, setDoc, docData, updateDoc } from "@angular/fire/firestore";
 
 import { UserService } from "../../auth/services/user.service";
 
-import { RoutineOUT } from "src/app/shared/interfaces/routine";
+import { Routine, RoutineOUT, RoutineExercise } from "src/app/shared/interfaces/routine";
+import { map, switchMap } from "rxjs";
 
 
 @Injectable({
@@ -21,6 +22,37 @@ export class RoutineService {
   ){}
 
 
+  get(_id: string){
+    return docData(doc(this.routineCollection, _id)).pipe(
+      switchMap(routine => {
+        const adminRef = doc(this.firestore, routine["admin"].path);
+        return docData(adminRef).pipe(
+          map(admin => {
+            admin["_id"] = adminRef.id;
+            routine["admin"] = admin;
+            return routine;
+          })
+        );
+      }),
+      switchMap(routine => {
+        const customerRef = doc(this.firestore, routine["customer"].path);
+        return docData(customerRef).pipe(
+          map(customer => {
+            customer["_id"] = customerRef.id;
+            routine["customer"] = customer;
+            return routine;
+          })
+        );
+      }),
+      map(routine => {
+        routine["dateIN"] = routine["dateIN"].toDate();
+        routine["dateOUT"] = routine["dateOUT"].toDate();
+        routine["_id"] = _id;
+        return routine as Routine;
+      })
+    );
+  }
+
   async create(fields: RoutineOUT, customer: string){
     const newDoc = doc(this.routineCollection);
     return setDoc(newDoc, {
@@ -31,5 +63,10 @@ export class RoutineService {
       this.snackBar.open("✅ Rutina creada correctamente");
       return newDoc.id;
     });
+  }
+
+  updateExercises(exercises: RoutineExercise[], routineID: string){
+    return updateDoc(doc(this.routineCollection, routineID), { exercises })
+      .then(() => { this.snackBar.open("✅ Rutina creada correctamente"); });
   }
 }
