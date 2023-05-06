4626e33f337d7314a446eeb418681c27b937ab9b
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-06 02:07:40 -0300

    feat(customer): :sparkles: get routine for customers in list of customers

--- src/app/modules/customer/pages/list/list.component.ts
+++ src/app/modules/customer/pages/list/list.component.ts
@@ -1,9 +1,11 @@
 import { Component } from "@angular/core";
-import { tap } from "rxjs";
+import { combineLatest, tap } from "rxjs";
 
 import { UserDocument } from "src/app/modules/auth/interfaces/user";
 
 import { CustomerService } from "../../services/customer.service";
+import { RoutineService } from "src/app/modules/routine/services/routine.service";
+import { Routine } from "src/app/shared/interfaces/routine";
 
 @Component({
   selector: "app-list",
@@ -15,12 +17,21 @@ export class ListComponent {
   customers: UserDocument[] = [];
 
   constructor(
-    private customerService: CustomerService
+    private customerService: CustomerService,
+    private routineService: RoutineService
   ){
-    this.customerService.customers.pipe(
+    combineLatest([this.customerService.customers, this.routineService.getList()]).pipe(
       tap(() => this.isLoading = false)
-    ).subscribe(customers => {
-      this.customers = customers;
+    ).subscribe(response => {
+      this.customers = response[0];
+      this.customers = this.customers.map(customer => {
+        const routine = response[1].filter(routine => {
+          return routine["customer"].id === customer._id;
+        })[0];
+
+        customer.routine = routine as Routine;
+        return customer;
+      });
     });
   }
 }
