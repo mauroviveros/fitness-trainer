880c92d5e06ae3eb80cc6f77fdb4ccf8ae89ddae
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-06 02:09:50 -0300

    feat(routine): :sparkles: add routing and imports

--- src/app/modules/routine/routine.module.ts
+++ src/app/modules/routine/routine.module.ts
@@ -6,12 +6,22 @@ import { SharedModule } from "src/app/shared/shared.module";
 
 import { CreateComponent } from "./pages/create/create.component";
 import { UpdateComponent } from "./pages/update/update.component";
+import { DetailComponent } from "./pages/detail/detail.component";
+import { AddExerciseDialogComponent } from "./components/add-exercise-dialog/add-exercise-dialog.component";
+import { DetailExerciseBottomSheetComponent } from "./components/detail-exercise-bottom-sheet/detail-exercise-bottom-sheet.component";
+import { DetailDialogComponent } from "./components/detail-dialog/detail-dialog.component";
+import { CompleteExerciseDialogComponent } from "./components/complete-exercise-dialog/complete-exercise-dialog.component";
 
 
 @NgModule({
   declarations: [
     CreateComponent,
-    UpdateComponent
+    UpdateComponent,
+    AddExerciseDialogComponent,
+    DetailExerciseBottomSheetComponent,
+    DetailComponent,
+    DetailDialogComponent,
+    CompleteExerciseDialogComponent
   ],
   imports: [
     CommonModule,
--- src/app/modules/routine/routine.routing.ts
+++ src/app/modules/routine/routine.routing.ts
@@ -3,19 +3,32 @@ import { RouterModule, Routes } from "@angular/router";
 
 import { CreateComponent } from "./pages/create/create.component";
 import { UpdateComponent } from "./pages/update/update.component";
+import { DetailComponent } from "./pages/detail/detail.component";
+import { AdminGuard } from "../auth/guards/admin.guard";
 
 const routes: Routes = [
   {
     path: "routine",
     children: [
       {
+        path: "",
+        component: DetailComponent
+      },
+      {
         path: "create",
+        canActivate: [AdminGuard],
         component: CreateComponent
       },
       {
         path: ":_id",
+        canActivate: [AdminGuard],
+        component: DetailComponent
+      },
+      {
+        path: ":_id/update",
+        canActivate: [AdminGuard],
         component: UpdateComponent
-      }
+      },
     ]
   }
 ];
