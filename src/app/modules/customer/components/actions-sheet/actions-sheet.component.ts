b0f8334ae2b5fb3767f0b73c0d04c32222bb6e34
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-06 02:11:07 -0300

    feat(customer): :sparkles: add actionsSheet for customers for administrate routines

--- src/app/modules/customer/components/actions-sheet/actions-sheet.component.ts
+++ src/app/modules/customer/components/actions-sheet/actions-sheet.component.ts
@@ -1,18 +1,42 @@
-import { Component } from "@angular/core";
-import { MatBottomSheetRef } from "@angular/material/bottom-sheet";
+import { Component, Inject, OnInit } from "@angular/core";
+import { MAT_BOTTOM_SHEET_DATA, MatBottomSheetRef } from "@angular/material/bottom-sheet";
 
 import { ActionSheet } from "src/app/shared/interfaces/interface";
+
+interface Action{
+  _id: ActionSheet,
+  icon: string,
+  text: string
+}
+
+interface Data{
+  hide:{
+    create: boolean,
+    update: boolean,
+    read  : boolean,
+    delete: boolean
+  }
+}
 
 @Component({
   selector: "app-actions-sheet",
   templateUrl: "./actions-sheet.component.html"
 })
-export class ActionsSheetComponent {
+export class ActionsSheetComponent implements OnInit {
+  actions: Action[] = [];
   constructor(
+    @Inject(MAT_BOTTOM_SHEET_DATA) private data: Data,
     private bottomSheetRef: MatBottomSheetRef<ActionsSheetComponent>
   ){}
 
-  action(action: ActionSheet){
+  ngOnInit(){
+    if(!this.data.hide.create) this.actions.push({ _id: "create", icon: "post_add", text: "crear rutina" });
+    if(!this.data.hide.update) this.actions.push({ _id: "update", icon: "edit", text: "actualizar rutina" });
+    if(!this.data.hide.read) this.actions.push({ _id: "read", icon: "preview", text: "visualizar rutina" });
+    if(!this.data.hide.delete) this.actions.push({ _id: "delete", icon: "delete", text: "borrar rutina" });
+  }
+
+  onAction(action: ActionSheet){
     this.bottomSheetRef.dismiss(action);
   }
 }
