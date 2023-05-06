1175800dda167406681334938be606f4dd1eeb80
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-06 14:25:08 -0300

    feat(core): :iphone: add responsive design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { SplashScreenService } from "./core/services/splash-screen.service";
     <core-splash-screen *ngIf="isLoading"></core-splash-screen>
     <router-outlet></router-outlet>
   `,
-  styles: [`:host{ display: flex; flex: 1; flex-direction: column; width: 100%; } `],
+  styles: [`:host{ display: flex; flex: 1; flex-direction: column; width: 100%; align-items: center; } `],
 })
 export class AppComponent {
   public isLoading = true;
