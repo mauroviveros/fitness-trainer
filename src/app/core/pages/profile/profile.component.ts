b4bc7423c6698778bd937def490a6087d87315cf
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-18 21:47:15 -0300

    feat(core): :sparkles: better profile component

--- src/app/core/pages/profile/profile.component.ts
+++ src/app/core/pages/profile/profile.component.ts
@@ -1,10 +1,73 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, inject } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { BehaviorSubject, Subscription, catchError, of, switchMap, tap } from "rxjs";
+
+import { AuthService } from "../../modules/auth/services/auth.service";
+import { UserService } from "../../modules/auth/services/user.service";
+
+import { UserDoc } from "src/app/shared/interfaces/user";
 
 @Component({
   selector: "core-profile",
   templateUrl: "./profile.component.html",
   styleUrls: ["./profile.component.scss"]
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  private readonly auth = inject(AuthService);
+  private readonly user = inject(UserService);
+  private readonly formBuider = inject(FormBuilder);
+  private readonly router = inject(Router);
+  private subscriptions: Subscription[] = [];
+
   isLoading = false;
+  mode = new BehaviorSubject<number>(3);
+
+  form: FormGroup = this.formBuider.group({
+    name: [null, [Validators.required]],
+    surname: [null, [Validators.required]],
+    email: [null, [Validators.required, Validators.email]]
+  });
+
+  ngOnInit(){
+    this.subscriptions.push(this.initMode());
+    this.subscriptions.push(this.initUserData());
+  }
+
+  private initMode(){
+    return this.mode.subscribe(mode => {
+      const controlsName = Object.keys(this.form.controls).filter(key => key !== "email");
+      controlsName.forEach(controlName => {
+        if(mode !== 3) this.form.controls[controlName].enable();
+        else this.form.controls[controlName].disable();
+      });
+    });
+  }
+
+  private initUserData(){
+    return this.auth.$user.pipe(
+      tap(({ email }) => {
+        this.form.controls["email"].setValue(email);
+        this.form.controls["email"].disable();
+      }),
+      switchMap(() => this.user.$data),
+      catchError(() => of({} as UserDoc))
+    ).subscribe((data) => {
+      if(!Object.keys(data).length) this.mode.next(1);
+
+      const controlsName = Object.keys(this.form.controls).filter(key => key !== "email");
+      controlsName.forEach(controlName => this.form.controls[controlName].setValue(data[controlName]));
+    });
+  }
+
+
+  submit(){
+    if(this.form.invalid) return;
+    
+    this.isLoading = true;
+    this.user.upload(this.form.value)
+      .then(() => { this.router.navigate([""]); })
+      .finally(() => this.isLoading = false);
+  }
+
 }
