88d107fbe966afae1c9be54e8727c2cfbdf48b60
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-19 22:15:43 -0300

    feat(core): :zap: improve profile experience

--- src/app/core/pages/profile/profile.component.ts
+++ src/app/core/pages/profile/profile.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit, inject } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { Router } from "@angular/router";
-import { BehaviorSubject, Subscription, catchError, of, switchMap, tap } from "rxjs";
+import { ActivatedRoute, Router } from "@angular/router";
+import { BehaviorSubject, Subscription, switchMap, tap } from "rxjs";
 
 import { AuthService } from "../../modules/auth/services/auth.service";
 import { UserService } from "../../modules/auth/services/user.service";
-
-import { UserDoc } from "src/app/shared/interfaces/user";
 
 @Component({
   selector: "core-profile",
@@ -18,6 +16,7 @@ export class ProfileComponent implements OnInit {
   private readonly user = inject(UserService);
   private readonly formBuider = inject(FormBuilder);
   private readonly router = inject(Router);
+  private readonly route = inject(ActivatedRoute);
   private subscriptions: Subscription[] = [];
 
   isLoading = false;
@@ -37,6 +36,7 @@ export class ProfileComponent implements OnInit {
   private initMode(){
     return this.mode.subscribe(mode => {
       const controlsName = Object.keys(this.form.controls).filter(key => key !== "email");
+
       controlsName.forEach(controlName => {
         if(mode !== 3) this.form.controls[controlName].enable();
         else this.form.controls[controlName].disable();
@@ -46,17 +46,17 @@ export class ProfileComponent implements OnInit {
 
   private initUserData(){
     return this.auth.$user.pipe(
-      tap(({ email }) => {
-        this.form.controls["email"].setValue(email);
+      tap(user => {
+        this.form.controls["email"].setValue(user?.email);
         this.form.controls["email"].disable();
       }),
-      switchMap(() => this.user.$data),
-      catchError(() => of({} as UserDoc))
-    ).subscribe((data) => {
-      if(!Object.keys(data).length) this.mode.next(1);
-
-      const controlsName = Object.keys(this.form.controls).filter(key => key !== "email");
-      controlsName.forEach(controlName => this.form.controls[controlName].setValue(data[controlName]));
+      switchMap(() => this.route.data),
+    ).subscribe(({ userData: data }) => {
+      if(!data) this.mode.next(1);
+      else{
+        const controlsName = Object.keys(this.form.controls).filter(key => key !== "email");
+        controlsName.forEach(controlName => this.form.controls[controlName].setValue(data[controlName]));
+      }
     });
   }
 
@@ -66,7 +66,10 @@ export class ProfileComponent implements OnInit {
     
     this.isLoading = true;
     this.user.upload(this.form.value)
-      .then(() => { this.router.navigate([""]); })
+      .then(() => {
+        this.form.markAsPristine();
+        this.router.navigate([""]);
+      })
       .finally(() => this.isLoading = false);
   }
 
