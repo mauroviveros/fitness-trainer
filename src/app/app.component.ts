d58b4791e63d6abdae776778afbd6e120ac6b253
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-19 22:21:05 -0300

    feat: :zap: improve splashScreen observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { Component, inject } from "@angular/core";
+import { Component, OnDestroy, OnInit, inject } from "@angular/core";
 
 import { AuthService } from "./core/modules/auth/services/auth.service";
 import { UserService } from "./core/modules/auth/services/user.service";
 
-import { finalize, switchMap, tap, of } from "rxjs";
+import { Subscription, switchMap, tap } from "rxjs";
 
 @Component({
   selector: "app-root",
@@ -13,16 +13,24 @@ import { finalize, switchMap, tap, of } from "rxjs";
   `,
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   private readonly auth = inject(AuthService);
   private readonly user = inject(UserService);
+  private subscription?: Subscription;
   isLoading = false;
 
   ngOnInit(){
-    this.auth.$user.pipe(
+    this.subscription = this.initLoadingChecker();
+  }
+
+  ngOnDestroy(){
+    this.subscription?.unsubscribe();
+  }
+
+  private initLoadingChecker(){
+    return this.auth.$authState.pipe(
       tap(() => this.isLoading = true),
       switchMap(() => this.user.$data),
-      finalize(() => this.isLoading = false)
-    ).subscribe();
+    ).subscribe(() => this.isLoading = false);
   }
 }
