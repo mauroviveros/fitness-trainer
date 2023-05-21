6fbcf94212209e51098f92b062ea0b90f4e06e84
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-21 02:43:37 -0300

    feat(core.auth): :heavy_plus_sign: add AngularMaterial modules
    adds MatChipsModule, MatListModule, MatMenuModule, MatSelectModule

--- src/app/shared/modules/material.module.ts
+++ src/app/shared/modules/material.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from "@angular/core";
 
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
+import { MatChipsModule } from "@angular/material/chips";
 import { MatDialogModule } from "@angular/material/dialog";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatIconModule } from "@angular/material/icon";
 import { MatInputModule } from "@angular/material/input";
+import { MatListModule } from "@angular/material/list";
+import { MatMenuModule } from "@angular/material/menu";
 import { MatProgressBarModule } from "@angular/material/progress-bar";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatSelectModule } from "@angular/material/select";
 import { MatSnackBarModule, MAT_SNACK_BAR_DEFAULT_OPTIONS } from "@angular/material/snack-bar";
 import { MatToolbarModule } from "@angular/material/toolbar";
 
@@ -15,12 +19,16 @@ import { MatToolbarModule } from "@angular/material/toolbar";
   exports: [
     MatButtonModule,
     MatCardModule,
+    MatChipsModule,
     MatDialogModule,
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
+    MatListModule,
+    MatMenuModule,
     MatProgressBarModule,
     MatProgressSpinnerModule,
+    MatSelectModule,
     MatSnackBarModule,
     MatToolbarModule
   ],
