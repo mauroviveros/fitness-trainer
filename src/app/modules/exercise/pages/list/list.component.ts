1cc38d926b2551a1ae539e9c34a3ada173284f59
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-23 21:54:00 -0300

    fix(exercise): :adhesive_bandage: update components with shared-layout

--- src/app/modules/exercise/pages/list/list.component.ts
+++ src/app/modules/exercise/pages/list/list.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, inject } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+
 import { ExerciseService } from "../../services/exercise.service";
 
 import { Exercise } from "src/app/shared/interfaces/exercise";
-import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: "exercise-list",
@@ -12,10 +13,12 @@ export class ListComponent implements OnInit{
   private readonly exercise = inject(ExerciseService);
   private readonly router = inject(Router);
   private readonly route = inject(ActivatedRoute);
+  isLoading = true;
   list: Exercise[] = [];
 
   ngOnInit(){
     this.exercise.$list.subscribe(exercises => {
+      this.isLoading = false;
       this.list = exercises;
     });
   }
