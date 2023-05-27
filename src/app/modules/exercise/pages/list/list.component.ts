1a63d578698d604a71cc6955eff05abb484966fe
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-27 19:15:34 -0300

    fix(exercise): :zap: add unsubscribe on listComponent

--- src/app/modules/exercise/pages/list/list.component.ts
+++ src/app/modules/exercise/pages/list/list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, inject } from "@angular/core";
+import { Component, OnDestroy, OnInit, inject } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
+import { Subscription } from "rxjs";
 
 import { ExerciseService } from "../../services/exercise.service";
 
@@ -9,19 +10,30 @@ import { Exercise } from "src/app/shared/interfaces/exercise";
   selector: "exercise-list",
   templateUrl: "./list.component.html"
 })
-export class ListComponent implements OnInit{
+export class ListComponent implements OnInit, OnDestroy {
   private readonly exercise = inject(ExerciseService);
   private readonly router = inject(Router);
   private readonly route = inject(ActivatedRoute);
+  private subscription?: Subscription;
   isLoading = true;
   list: Exercise[] = [];
 
   ngOnInit(){
-    this.exercise.$list.subscribe(exercises => {
+    this.subscription = this.initList();
+  }
+
+  ngOnDestroy(){
+    this.subscription?.unsubscribe();
+  }
+
+  private initList(){
+    return this.exercise.$list.subscribe(exercises => {
       this.isLoading = false;
       this.list = exercises;
     });
   }
 
-  create(){ this.router.navigate(["create"], { relativeTo: this.route }); }
+  create(){
+    this.router.navigate(["create"], { relativeTo: this.route });
+  }
 }
