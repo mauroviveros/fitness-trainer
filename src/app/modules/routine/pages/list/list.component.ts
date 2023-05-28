01734e380b171842db330c10ee1476dbf017efc2
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-28 02:45:53 -0300

    feat(routine): :zap: use service for listComponent

--- src/app/modules/routine/pages/list/list.component.ts
+++ src/app/modules/routine/pages/list/list.component.ts
@@ -1,13 +1,37 @@
-import { Component, inject } from "@angular/core";
+import { Component, OnDestroy, OnInit, inject } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
+import { RoutineService } from "../../services/routine.service";
+import { Subscription } from "rxjs";
+import { Routine } from "src/app/shared/interfaces/routine";
 
 @Component({
   selector: "routine-list",
   templateUrl: "./list.component.html"
 })
-export class ListComponent {
+export class ListComponent implements OnInit, OnDestroy {
   private readonly router = inject(Router);
   private readonly route = inject(ActivatedRoute);
+  private readonly routine = inject(RoutineService);
+  subscription?: Subscription;
+  isLoading = false;
+  list: Routine[] = [];
+
+
+  ngOnInit(){
+    this.subscription = this.initList();
+  }
+
+  ngOnDestroy(){
+    this.subscription?.unsubscribe();
+  }
+
+  private initList(){
+    this.isLoading = true;
+    return this.routine.$list.subscribe(routines => {
+      this.isLoading = false;
+      this.list = routines;
+    });
+  }
 
   create(){ this.router.navigate(["create"], { relativeTo: this.route }); }
 }
