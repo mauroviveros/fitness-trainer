70ef59125e10bd8111dc2801bc79672ad04af30f
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-28 02:04:47 -0300

    feat(routine): :sparkles: save admin and customer reference on upload

--- src/app/modules/routine/services/routine.service.ts
+++ src/app/modules/routine/services/routine.service.ts
@@ -1,21 +1,28 @@
 import { Injectable, inject } from "@angular/core";
 import { Firestore, collection, doc, setDoc } from "@angular/fire/firestore";
 
-import { Routine } from "src/app/shared/interfaces/routine";
 import { MessageService } from "src/app/shared/services/message.service";
+
+import { Routine } from "src/app/shared/interfaces/routine";
+import { UserService } from "src/app/core/modules/auth/services/user.service";
 
 @Injectable({
   providedIn: "root"
 })
 export class RoutineService {
+  private readonly user = inject(UserService);
+  private readonly message = inject(MessageService);
   private readonly firestore = inject(Firestore);
   private readonly collection = collection(this.firestore, "routines");
-  private readonly message = inject(MessageService);
 
   async upload(fields: Routine){
     try {
+      const { admin, customer, ...routine } = fields;
+      const adminRef = this.user.ref(admin);
+      const customerRef = this.user.ref(customer);
       const routineDoc = doc(this.collection);
-      await setDoc(routineDoc, { ...fields });
+
+      await setDoc(routineDoc, { admin: adminRef, customer: customerRef,...routine });
       this.message.success("Rutina generada correctamente");
       return routineDoc.id;
     } catch (error) {
