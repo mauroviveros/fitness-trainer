70bd58079374fac2648e343c6ec7cb4634c9d41c
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-28 02:38:18 -0300

    feat(customer): :adhesive_bandage: use user.convert to conver customers users

--- src/app/modules/customer/services/customer.service.ts
+++ src/app/modules/customer/services/customer.service.ts
@@ -1,23 +1,17 @@
 import { Injectable, inject } from "@angular/core";
 import { Firestore, collection, collectionData, query, where } from "@angular/fire/firestore";
 import { map } from "rxjs";
-
-import { UserDoc } from "src/app/shared/interfaces/user";
+import { UserService } from "src/app/core/modules/auth/services/user.service";
 
 @Injectable({
   providedIn: "root"
 })
 export class CustomerService {
+  private readonly user = inject(UserService);
   private readonly firestore = inject(Firestore);
   private readonly collection = collection(this.firestore, "users");
 
-  readonly $list = collectionData(query(this.collection, where("admin", "!=", true)), { idField: "_id" }).pipe(
-    map(list => {
-      return list.map(customer => {
-        if(customer["birthday"]) customer["birthday"] = customer["birthday"].toDate();
-
-        return customer;
-      }) as UserDoc[];
-    })
+  readonly $list = collectionData(query(this.collection, where("_admin", "!=", true)), { idField: "_id" }).pipe(
+    map(list => list.map(customer => this.user.convert(customer)))
   );
 }
