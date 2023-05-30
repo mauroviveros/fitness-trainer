e01210b598a63719ead83870cd503ba0054b47e7
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-30 15:46:59 -0300

    feat(routine): :truck: move form and use detailFormComponent

--- src/app/modules/routine/pages/create/create.component.ts
+++ src/app/modules/routine/pages/create/create.component.ts
@@ -1,15 +1,10 @@
 import { Component, OnDestroy, OnInit, inject } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { MatCalendarCellClassFunction } from "@angular/material/datepicker";
+import { Router } from "@angular/router";
 import { Subscription } from "rxjs";
 
-import { DateUtilsService } from "src/app/shared/services/date-utils.service";
 import { AuthService } from "src/app/core/modules/auth/services/auth.service";
-import { CustomerService } from "src/app/modules/customer/services/customer.service";
+import { RoutineUtilsService } from "../../services/routine-utils.service";
 import { RoutineService } from "../../services/routine.service";
-import { Router } from "@angular/router";
-
-const levels = ["BAJA", "MEDIA", "INTENSA", "MUY INTENSA"];
 
 @Component({
   selector: "routine-create",
@@ -17,32 +12,15 @@ const levels = ["BAJA", "MEDIA", "INTENSA", "MUY INTENSA"];
   styleUrls: ["./create.component.scss"]
 })
 export class CreateComponent implements OnInit, OnDestroy {
-  private readonly formBuilder = inject(FormBuilder);
-  private readonly customer = inject(CustomerService);
+  private readonly routineUtils = inject(RoutineUtilsService);
   private readonly routine = inject(RoutineService);
   private readonly auth = inject(AuthService);
   private readonly router = inject(Router);
-  readonly dateUtils = inject(DateUtilsService);
   private subscription?: Subscription;
+  isLoading = false;
   isSaving = false;
 
-
-  readonly levels = levels;
-  readonly $customers = this.customer.$list;
-  readonly form: FormGroup = this.formBuilder.group({
-    admin: [null, [Validators.required]],
-    days: [null, [Validators.required]],
-    dateIN: [null, [Validators.required]],
-    dateOUT: [null, [Validators.required]],
-    customer: [null, [Validators.required]],
-    level: [null, [Validators.required]],
-    kal: [null, [Validators.required]],
-    objective: [null, [Validators.required]]
-  });
-
-  get chips(){
-    return this.dateUtils.getDays(this.form.controls["days"].value);
-  }
+  readonly form = this.routineUtils.createForm();
 
   ngOnInit(){
     this.subscription = this.initAdminField();
@@ -69,19 +47,6 @@ export class CreateComponent implements OnInit, OnDestroy {
     });
   }
 
-  highlightSelected: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
-    const ctrlValue = this.form.controls["days"].value;
-    if(view !== "month" || !ctrlValue) return "";
-    const date = cellDate.getDay();
-    return ctrlValue.includes(date) ? "day_selected" : "";
-  };
-
-  onRemovedChip(indexRemoved: number){
-    const ctrl = this.form.controls["days"];
-    const value = ctrl.value.filter((_: number, i: number) => i !== indexRemoved);
-    ctrl.setValue(value);
-  }
-
 
   submit(){
     if(this.form.invalid) return;
@@ -89,7 +54,10 @@ export class CreateComponent implements OnInit, OnDestroy {
 
     this.toggleForm(true);
     this.routine.upload(value)
-      .then(routineID => this.router.navigate(["routines", routineID, "edit"]))
+      .then(routineID => {
+        this.form.markAsPristine();
+        this.router.navigate(["routines", routineID, "edit"]);
+      })
       .finally(() => this.toggleForm(false));
   }
 }
