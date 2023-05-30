8c629f22033769d9ec03494276d4aaf5de48c39d
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-30 15:38:48 -0300

    feat(core.shared): :label: add interface RoutineOUT

--- src/app/shared/interfaces/routine.ts
+++ src/app/shared/interfaces/routine.ts
@@ -1,3 +1,4 @@
+import { UserDoc } from "./user";
 
 type daysOfWeek = 0 | 1 | 2 | 3 | 4 | 5 | 6;
 // 0 domingo, 1 lunes, 2 martes, 3 miercoles, 4 jueves, 5 viernes, 6 sabado
@@ -7,12 +8,25 @@ type levelObjetive = 0 | 1 | 2 | 3;
 
 export interface Routine {
     _id: string
-    admin: string
-    customer: string
-    dateIN: Date
-    dateOUT: Date
-    days: daysOfWeek[]
+    admin: UserDoc
+    customer: UserDoc
+    objective: string,
     kal: number
     level: levelObjetive
+    days: daysOfWeek[]
+    dateIN: Date
+    dateOUT: Date
+    [key: string]: string | Date | number | daysOfWeek[] | UserDoc
+}
+
+export interface RoutineOUT {
+    _id: string
+    admin: string
+    customer: string
     objective: string
+    kal: number
+    level: number
+    days: number[]
+    dateIN: Date
+    dateOUT: Date
 }
