7e82b8a6957225c082c40626ae8e4da1b018fb4e
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-10-06 12:44:08 -0300

    fix(core.auth): :iphone: fix layout to guests and add wrapperComponent

--- src/app/core/modules/auth/components/layout/layout.component.ts
+++ src/app/core/modules/auth/components/layout/layout.component.ts
@@ -1,37 +1,20 @@
-import { Component, OnDestroy, OnInit, inject } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { Subscription } from "rxjs";
 
-interface Footer{
-  label: string
-  button: string,
-  link: string
-}
 interface Layout{
   title: string
-  footer?: Footer
+  footer?: {
+    label: string
+    button: string,
+    link: string
+  }
 }
-
 @Component({
   selector: "auth-layout",
   templateUrl: "./layout.component.html",
   styleUrls: ["./layout.component.scss"]
 })
-export class LayoutComponent implements OnInit, OnDestroy {
+export class LayoutComponent {
   private readonly route = inject(ActivatedRoute);
-  private subscription?: Subscription;
-  layout?: Layout;
-
-  ngOnInit(){
-    this.subscription = this.initLayout();
-  }
-  ngOnDestroy(){
-    this.subscription?.unsubscribe();
-  }
-
-  private initLayout(){
-    return this.route.data.subscribe(({ layout }) => {
-      this.layout = layout;
-    });
-  }
+  layout: Layout = this.route.snapshot.data["layout"];
 }
---
+++ src/app/core/modules/auth/components/wrapper/wrapper.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "auth-wrapper",
+  templateUrl: "./wrapper.component.html",
+  styles: [":host{ width: 100% !important; height: 100% !important; }"]
+})
+export class WrapperComponent {}
