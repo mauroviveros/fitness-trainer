01a41fbdd68627d043e7a42b1fc65f6a224a2407
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-10-06 12:28:12 -0300

    fix(core.auth): :wheelchair: better emailGuard

--- src/app/core/modules/auth/guards/email.guard.ts
+++ src/app/core/modules/auth/guards/email.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from "@angular/core";
-import { UrlTree } from "@angular/router";
-import { Observable, map } from "rxjs";
+import { Router, UrlTree } from "@angular/router";
+import { Observable, map, tap } from "rxjs";
 
 import { AuthService } from "../services/auth.service";
 import { DialogService } from "src/app/shared/services/dialog.service";
@@ -9,18 +9,15 @@ import { DialogService } from "src/app/shared/services/dialog.service";
   providedIn: "root"
 })
 export class EmailGuard {
-  private readonly auth = inject(AuthService);
+  private readonly router = inject(Router);
+  private readonly auth   = inject(AuthService);
   private readonly dialog = inject(DialogService);
 
   canActivate(): Observable<boolean | UrlTree>{
     return this.auth.$user.pipe(
-      map(user => {
-        if(user !== null && !user?.emailVerified){
-          this.dialog.showEmailValidation(user).subscribe();
-          this.auth.logout();
-        }
-        return user?.emailVerified || false;
-      }),
+      tap(user => user.emailVerified ? null : this.dialog.showEmailValidation(user).subscribe()),
+      tap(user => user.emailVerified ? null : this.auth.logout()),
+      map(user => user.emailVerified)
     );
   }
   
