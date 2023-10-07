9f4eeffd479d4ec6aaa86baf916ad92ff16db1d4
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-10-07 05:52:30 -0300

    feat(customer): :sparkles: add customerService

--- src/app/modules/customer/services/customer.service.ts
+++ src/app/modules/customer/services/customer.service.ts
@@ -1,17 +1,22 @@
 import { Injectable, inject } from "@angular/core";
 import { Firestore, collection, collectionData, query, where } from "@angular/fire/firestore";
-import { map } from "rxjs";
+import { MatDialog } from "@angular/material/dialog";
+import { Observable, map } from "rxjs";
 import { UserService } from "src/app/core/modules/auth/services/user.service";
+import { UserDoc } from "src/app/shared/interfaces/user";
+import { MessageService } from "src/app/shared/services/message.service";
 
 @Injectable({
   providedIn: "root"
 })
 export class CustomerService {
-  private readonly user = inject(UserService);
   private readonly firestore = inject(Firestore);
   private readonly collection = collection(this.firestore, "users");
+  private readonly dialog = inject(MatDialog);
+  private readonly user = inject(UserService);
+  private readonly message = inject(MessageService);
 
-  readonly $list = collectionData(query(this.collection, where("_admin", "!=", true)), { idField: "_id" }).pipe(
-    map(list => list.map(customer => this.user.convert(customer)))
+  readonly $list : Observable<UserDoc[]> = collectionData(query(this.collection,  where("_admin", "==", false)), { idField: "_id" }).pipe(
+    map(list => list.map(customer => this.user.convert(customer) as UserDoc))
   );
 }
