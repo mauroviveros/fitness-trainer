f82da4062a891e182ba80a2d19fd992fa1571c02
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-10-07 20:28:28 -0300

    feat(customer): :sparkles: add crateRoutine method

--- src/app/modules/customer/components/item/item.component.ts
+++ src/app/modules/customer/components/item/item.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, inject } from "@angular/core";
+import { RoutineService } from "src/app/modules/routine/services/routine.service";
+import { Action } from "src/app/shared/interfaces/interfaces";
 
 import { UserDoc } from "src/app/shared/interfaces/user";
 
@@ -7,5 +9,17 @@ import { UserDoc } from "src/app/shared/interfaces/user";
   templateUrl: "./item.component.html"
 })
 export class ItemComponent {
+  private readonly routine = inject(RoutineService);
+
   @Input() customer!: UserDoc;
+
+  actions: Action[] = [
+    { _id: "create", icon: "playlist_play", text: "Crear Rutina" },
+  ];
+
+  onAction(action: string){
+    switch(action){
+      case "create" : this.routine.openRoutine(1, { customer: this.customer }) ; break;
+    }
+  }
 }
