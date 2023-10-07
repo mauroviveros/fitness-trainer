138d814e9e274e6976ef38e39305696b3e3cbd54
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-10-07 05:05:51 -0300

    fix(exercise): :coffin: remove useless routes

--- src/app/modules/exercise/exercise.module.ts
+++ src/app/modules/exercise/exercise.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from "@angular/core";
-
 import { SharedModule } from "src/app/shared/shared.module";
 import { ExerciseRoutingModule } from "./exercise.routing";
 
-import { ItemListComponent } from "./components/item-list/item-list.component";
 import { ListComponent } from "./pages/list/list.component";
-import { DetailComponent } from "./pages/detail/detail.component";
+import { ItemComponent } from "./components/item/item.component";
+import { DetailComponent } from "./components/detail/detail.component";
+
 
 
 @NgModule({
   declarations: [
-    ItemListComponent,
     ListComponent,
+    ItemComponent,
     DetailComponent
   ],
   imports: [
--- src/app/modules/exercise/exercise.routing.ts
+++ src/app/modules/exercise/exercise.routing.ts
@@ -2,60 +2,12 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 import { ListComponent } from "./pages/list/list.component";
-import { DetailComponent } from "./pages/detail/detail.component";
 
 const routes: Routes = [
   {
     path: "",
-    title: "Fitness - Trainer | Ejercicios Listado",
-    component: ListComponent,
-    data: {
-      layout: {
-        title: "Listado Ejercicios"
-      }
-    }
-  },
-  {
-    path: "create",
-    title: "Fitness - Trainer | Crear Ejercicio",
-    component: DetailComponent,
-    data: {
-      layout: {
-        title: "Crear ejercicio",
-        close: true,
-        actions: [
-          { _id: "save", icon: "save", type: "submit" }
-        ]
-      }
-    }
-  },
-  {
-    path: ":_id",
-    title: "Fitness - Trainer | Detalle Ejercicio",
-    component: DetailComponent,
-    data: {
-      layout: {
-        title: "Detalle ejercicio",
-        back: true,
-        actions: [
-          { _id: "edit", icon: "edit" }
-        ]
-      }
-    }
-  },
-  {
-    path: ":_id/edit",
-    title: "Fitness - Trainer | Editar Ejercicio",
-    component: DetailComponent,
-    data: {
-      layout: {
-        title: "Detalle ejercicio",
-        close: true,
-        actions: [
-          { _id: "save", icon: "save", type: "submit" }
-        ]
-      }
-    }
+    title: "Fitness - Trainer | Listado Ejercicios",
+    component: ListComponent
   }
 ];
 
