9fe6d64fb67c929247e93d23d0e7f6e39e1d75ad
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-10-07 23:51:15 -0300

    feat(customer): :sparkles: get Routines and add logic with them

--- src/app/modules/customer/components/item/item.component.ts
+++ src/app/modules/customer/components/item/item.component.ts
@@ -1,25 +1,36 @@
 import { Component, Input, inject } from "@angular/core";
 import { RoutineService } from "src/app/modules/routine/services/routine.service";
 import { Action } from "src/app/shared/interfaces/interfaces";
+import { Routine } from "src/app/shared/interfaces/routine";
 
 import { UserDoc } from "src/app/shared/interfaces/user";
 
 @Component({
   selector: "customer-item",
-  templateUrl: "./item.component.html"
+  templateUrl: "./item.component.html",
+  styleUrls: ["./item.component.scss"]
 })
 export class ItemComponent {
-  private readonly routine = inject(RoutineService);
+  private readonly routineSrv = inject(RoutineService);
 
   @Input() customer!: UserDoc;
+  @Input() routines: Routine[] = [];
 
-  actions: Action[] = [
-    { _id: "create", icon: "playlist_play", text: "Crear Rutina" },
-  ];
+  get actions() : Action[] {
+    let actions : Action[] = [
+      { _id: "create", icon: "playlist_play", text: "Crear Rutina" },
+      { _id: "history", icon: "history", text: "Historial de rutinas" }
+    ];
+
+    if(this.routines.length) actions = actions.filter(action => action._id !== "create");
+    if(this.routines.length <= 1) actions = actions.filter(action => action._id !== "history");
+
+    return actions;
+  }
 
   onAction(action: string){
     switch(action){
-      case "create" : this.routine.openRoutine(1, { customer: this.customer }) ; break;
+      case "create" : this.routineSrv.openRoutine(1, undefined, this.customer) ; break;
     }
   }
 }
--- src/app/modules/customer/pages/list/list.component.ts
+++ src/app/modules/customer/pages/list/list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnDestroy, OnInit, inject } from "@angular/core";
 import { Subscription, tap } from "rxjs";
 import { UserDoc } from "src/app/shared/interfaces/user";
 import { CustomerService } from "../../services/customer.service";
+import { RoutineService } from "src/app/modules/routine/services/routine.service";
+import { Routine } from "src/app/shared/interfaces/routine";
 
 @Component({
   selector: "customer-list",
@@ -9,14 +11,17 @@ import { CustomerService } from "../../services/customer.service";
 })
 export class ListComponent implements OnInit, OnDestroy {
   private readonly customer = inject(CustomerService);
+  private readonly routine = inject(RoutineService);
   private readonly subscriptions : Subscription[] = [];
   customers : UserDoc[] = [];
+  routines  : Routine[] = [];
 
   isLoadingDM = true;
   isLoading = false;
 
   ngOnInit(){
     this.subscriptions.push(this.initList());
+    this.subscriptions.push(this.initRoutines());
   }
 
   ngOnDestroy(){
@@ -27,5 +32,12 @@ export class ListComponent implements OnInit, OnDestroy {
     return this.customer.$list.pipe(
       tap(() => this.isLoadingDM = false)
     ).subscribe(customers => this.customers = customers);
+  }
+  private initRoutines(){
+    return this.routine.$list.subscribe(routines => this.routines = routines);
+  }
+
+  getRoutines(customer: UserDoc){
+    return this.routines.filter(routine => routine.customer.id === customer._id);
   }
 }
