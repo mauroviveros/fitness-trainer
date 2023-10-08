afac1e3331f08c98f39290c4cd971025893ab4e6
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-10-08 14:42:42 -0300

    feat(core.shared): :sparkles: add emptyBannerComponent

---
+++ src/app/shared/components/empty-banner/empty-banner.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "shared-empty-banner",
+  templateUrl: "./empty-banner.component.html",
+  styleUrls: ["./empty-banner.component.scss"]
+})
+export class EmptyBannerComponent {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,37 +2,46 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
 import { ReactiveFormsModule } from "@angular/forms";
-
 import { MaterialModule } from "./modules/material.module";
 
 import { ErrorPipe } from "./pipes/error.pipe";
-import { CustomerPipe } from "./pipes/customer.pipe";
+import { UserPipe } from "./pipes/user.pipe";
 
 import { DialogComponent } from "./components/dialog/dialog.component";
 import { VideoDialogComponent } from "./components/video-dialog/video-dialog.component";
 import { LayoutComponent } from "./components/layout/layout.component";
+import { ActionsComponent } from "./components/actions/actions.component";
+import { ActionsSheetComponent } from "./components/actions-sheet/actions-sheet.component";
+import { EmptyBannerComponent } from "./components/empty-banner/empty-banner.component";
 
 @NgModule({
   declarations: [
-    ErrorPipe,
     DialogComponent,
     VideoDialogComponent,
     LayoutComponent,
-    CustomerPipe
+    ActionsComponent,
+    ActionsSheetComponent,
+    EmptyBannerComponent,
+    ErrorPipe,
+    UserPipe,
   ],
   imports: [
     CommonModule,
     RouterModule,
+    ReactiveFormsModule,
     MaterialModule
   ],
   exports: [
     CommonModule,
-    ReactiveFormsModule,
     RouterModule,
+    ReactiveFormsModule,
     MaterialModule,
+    DialogComponent,
+    LayoutComponent,
+    ActionsComponent,
+    EmptyBannerComponent,
     ErrorPipe,
-    CustomerPipe,
-    LayoutComponent
+    UserPipe
   ]
 })
 export class SharedModule { }
