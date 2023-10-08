6183c89475d03132f997a3b2141ce7ef0cc8c1ca
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-10-08 20:25:54 -0300

    feat(routine): :children_crossing: if don't exist routine go back

--- src/app/modules/routine/pages/detail/detail.component.ts
+++ src/app/modules/routine/pages/detail/detail.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnDestroy, OnInit, inject } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Subscription, tap } from "rxjs";
 
 import { RoutineService } from "../../services/routine.service";
 
 import { Routine } from "src/app/shared/interfaces/routine";
-import { Subscription } from "rxjs";
 
 @Component({
   selector: "routine-detail",
@@ -12,18 +12,25 @@ import { Subscription } from "rxjs";
   styleUrls: ["./detail.component.scss"]
 })
 export class DetailComponent implements OnInit, OnDestroy {
+  private readonly router = inject(Router);
   private readonly route = inject(ActivatedRoute);
   private readonly RoutineSrv = inject(RoutineService);
   private readonly subscriptions : Subscription[] = [];
   routine? : Routine;
+
+  isLoading = true;
 
   ngOnInit(){
     this.subscriptions.push(this.initRoutine());
   }
 
   private initRoutine(){
-    return this.RoutineSrv.detail(this.route.snapshot.params["_id"])
-      .subscribe(routine => this.routine = routine);
+    return this.RoutineSrv.detail(this.route.snapshot.params["_id"]).pipe(
+      tap(() => this.isLoading = false)
+    ).subscribe({
+      error: () => this.router.navigate(["/customers"]),
+      next: routine => this.routine = routine
+    });
   }
 
   ngOnDestroy(){
