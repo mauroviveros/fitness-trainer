89c6113d697855957b5cac8810adf9f613e6530c
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-10-07 23:43:25 -0300

    feat(core.shared): :sparkles: add userPipe

---
+++ src/app/shared/pipes/user.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from "@angular/core";
+import { UserDoc } from "../interfaces/user";
+
+@Pipe({
+  name: "user"
+})
+export class UserPipe implements PipeTransform {
+
+  transform(user: UserDoc): string {
+    return `${user.name} ${user.surname}`;
+  }
+
+}
