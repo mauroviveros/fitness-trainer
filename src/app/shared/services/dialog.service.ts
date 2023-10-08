f9429c6bad79105d2b6e619a1d132d0756721b73
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-10-08 14:16:43 -0300

    feat(core.shared): :sparkles: refactor dialogService

--- src/app/shared/services/dialog.service.ts
+++ src/app/shared/services/dialog.service.ts
@@ -1,14 +1,14 @@
 import { Injectable, inject } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
 import { User } from "@angular/fire/auth";
-import { map, tap } from "rxjs";
+import { Observable, map, tap } from "rxjs";
 
 import { AuthService } from "src/app/core/modules/auth/services/auth.service";
 
 import { DialogComponent } from "../components/dialog/dialog.component";
+import { VideoDialogComponent } from "../components/video-dialog/video-dialog.component";
 
 import { DialogContent } from "../interfaces/dialog";
-import { VideoDialogComponent } from "../components/video-dialog/video-dialog.component";
 
 @Injectable({
   providedIn: "root"
@@ -17,12 +17,13 @@ export class DialogService {
   private readonly auth = inject(AuthService);
   private readonly dialog = inject(MatDialog);
 
-  open(dialogContent: DialogContent){
-    return this.dialog.open(DialogComponent, { data: dialogContent })
-      .afterClosed().pipe(map(action => action === dialogContent.action?._id));
+  open(dialogContent: DialogContent) : Observable<boolean> {
+    return this.dialog.open(DialogComponent, { data: dialogContent }).afterClosed().pipe(
+      map(action => action === dialogContent.action?._id)
+    );
   }
 
-  openVideoFrame(title: string, url: string){
+  openVideoFrame(title: string, url: string) : Observable<boolean> {
     return this.dialog.open(VideoDialogComponent, {
       data: { title, url },
       width: "calc(100% - 2rem)",
@@ -31,33 +32,22 @@ export class DialogService {
     }).afterClosed();
   }
 
-  showEmailValidation(user: User){
+  showEmailValidation(user: User) : Observable<boolean> {
     return this.open({
       title: "Verifica tu email",
       icon: "security",
       texts: ["Tu email no ha sido verificado todavía. Por favor verifica tu dirección de correo electrónico para acceder a todas las funcionalidades de nuestra plataforma."],
-      action: { _id: "sendEmail", label: "Enviar Email" }
+      action: { _id: "sendEmail", label: "Re Enviar Email" }
     }).pipe(
-      tap(boolean => { if(boolean) this.auth.sendEmailVerification(user); })
+      tap(boolean => {
+        if(boolean) this.auth.sendEmailVerification(user);
+        else this.auth.logout();
+      })
     );
   }
 
-  showMandatoryProfile(){
+  showWelcome() : Observable<boolean> {
     return this.open({
-      title: "¡Formulario obligatorio!",
-      icon: "error",
-      texts: [
-        "Es obligatorio completar el formulario antes de poder continuar. Por favor, proporciona la información requerida para continuar",
-        "Si lo desea puede cerrar la sesión"
-      ],
-      action: { _id: "logout", label: "Cerrar Sesión" }
-    }).pipe(
-      tap(boolean => { if(boolean) this.auth.logout(); })
-    );
-  }
-
-  showWelcome(){
-    this.open({
       title: "¡Bienvenido!",
       icon: "waving_hand",
       texts: [
@@ -67,7 +57,7 @@ export class DialogService {
     });
   }
 
-  showUnsavedChanges(){
+  showUnsavedChanges() : Observable<boolean> {
     return this.open({
       title: "¡Cambios no guardados!",
       icon: "error",
@@ -79,15 +69,16 @@ export class DialogService {
     });
   }
 
-  showConfirmDelete(title: string, docName: string){
+  showConfirmDelete(title: string, document: string) : Observable<boolean> {
     return this.open({
       title,
       icon: "delete",
       texts: [
-        `Vas a borrar: "${docName}".`,
+        `Vas a borrar: "${document}".`,
         "¿Estas seguro de que desea borrarlo?"
       ],
       action: { _id: "confirm", label: "Confirmar" }
     });
   }
+
 }
