fb51744a5bdbe921c555537b0faaa09cea30ff74
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-10-07 23:47:26 -0300

    feat(routine): :sparkles: better routineService

--- src/app/modules/routine/services/routine.service.ts
+++ src/app/modules/routine/services/routine.service.ts
@@ -1,78 +1,65 @@
 import { Injectable, inject } from "@angular/core";
-import { DocumentData, Firestore, collection, collectionData, doc, docData, query, setDoc, where } from "@angular/fire/firestore";
-import { filter, from, map, of, switchMap, take, tap } from "rxjs";
+import { DocumentData, Firestore, collection, collectionData, doc, orderBy, query, setDoc } from "@angular/fire/firestore";
+import { MatDialog, MatDialogRef } from "@angular/material/dialog";
+import { Observable, map } from "rxjs";
 
-import { MessageService } from "src/app/shared/services/message.service";
-
-import { Routine, RoutineOUT } from "src/app/shared/interfaces/routine";
 import { UserService } from "src/app/core/modules/auth/services/user.service";
+import { MessageService } from "src/app/shared/services/message.service";
+import { DetailDialogComponent } from "../components/detail-dialog/detail-dialog.component";
+
+import { Routine } from "src/app/shared/interfaces/routine";
 import { UserDoc } from "src/app/shared/interfaces/user";
 
 @Injectable({
   providedIn: "root"
 })
 export class RoutineService {
-  private readonly user = inject(UserService);
-  private readonly message = inject(MessageService);
   private readonly firestore = inject(Firestore);
   private readonly collection = collection(this.firestore, "routines");
+  private readonly dialog = inject(MatDialog);
+  private readonly user = inject(UserService);
+  private readonly message = inject(MessageService);
 
-  readonly $list = collectionData(this.collection, { idField: "_id" }).pipe(
-    switchMap(routines => this.convert(routines).pipe(take(1)))
+  private userData?: UserDoc;
+
+  readonly levels = ["Baja", "Media", "Intensa", "Muy intensa"];
+  readonly $list : Observable<Routine[]> = collectionData(query(this.collection, orderBy("dateIN", "desc")), { idField: "_id" }).pipe(
+    map(routines => routines.map(routine => this.convert(routine) as Routine))
   );
 
-  get(_id: string){
-    return docData(doc(this.collection, _id), { idField: "_id" }).pipe(
-      switchMap(routine => this.convert([routine]).pipe(take(1))),
-      map(routines => routines[0])
-    );
+  constructor(){
+    this.user.$data.subscribe(user => { this.userData = user; });
   }
 
-  getOwn(){
-    return this.user.$data.pipe(
-      filter(user => user !== null && user !== undefined),
-      map(user => user as UserDoc),
-      switchMap(user => collectionData(query(this.collection, where("customer", "==", this.user.ref(user._id))))),
-      switchMap(routines => this.convert(routines).pipe(take(1))),
-      map(routines => routines[0])
-    );
+  private convert(document: DocumentData) : Routine{
+    if(document["dateIN"]) document["dateIN"] = document["dateIN"].toDate();
+    if(document["dateOUT"]) document["dateOUT"] = document["dateOUT"].toDate();
+    return document as Routine;
   }
 
-  private convert(routines: DocumentData[]){
-    return of(routines).pipe(
-      map(routines => routines.map(routine => {
-        routine["dateIN"] = routine["dateIN"].toDate();
-        routine["dateOUT"] = routine["dateOUT"].toDate();
-        return routine;
-      })),
-      switchMap(routines => from(routines).pipe(
-        switchMap(routine => this.user.doc(routine["admin"]).pipe(
-          map(admin => ({ ...routine, admin } as DocumentData))
-        )),
-        switchMap(routine => this.user.doc(routine["customer"]).pipe(
-          map(customer => ({ ...routine, customer } as DocumentData))
-        )),
-        map((routine, index) => {
-          routines[index] = routine;
-          return routines;
-        })
-      )),
-      map(routines => routines as Routine[])
-    );
+  openRoutine(mode: 1 | 2 | 3 = 3, routine?: Routine, customer?: UserDoc) : MatDialogRef<DetailDialogComponent> {
+    const data = { mode, routine: routine || {} as Routine };
+
+    if(!routine){
+      if(this.userData) data.routine.admin = this.user.ref(this.userData);
+      if(customer) data.routine.customer = this.user.ref(customer);
+    }
+
+    return this.dialog.open(DetailDialogComponent, { disableClose: true, data });
   }
 
-  async upload(fields: RoutineOUT){
+
+  async upload(fields: Routine) : Promise<void> {
     try {
       const { admin, customer, ...routine } = fields;
-      const adminRef = this.user.ref(admin);
-      const customerRef = this.user.ref(customer);
-      const routineDoc = doc(this.collection);
 
-      await setDoc(routineDoc, { admin: adminRef, customer: customerRef,...routine });
-      this.message.success("Rutina generada correctamente");
-      return routineDoc.id;
+      // const adminRef = this.user.ref(admin);
+      // const customerRef = this.user.ref(customer);
+
+      await setDoc(doc(this.collection), { admin, customer, ...routine });
+      this.message.success("Rutina creada correctamente");
     } catch (error) {
-      this.message.error(error as Error);
+      if(error instanceof Error){ this.message.error(error); }
       throw error;
     }
   }
