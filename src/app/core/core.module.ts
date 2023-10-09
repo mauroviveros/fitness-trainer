1d0007c78c124e4d37fe368e81431918e9f3a2fe
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-10-08 21:54:14 -0300

    fix(core): :adhesive_bandage: rename sharedProfileImageComponent

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from "@angular/core";
 
 import { AuthModule } from "./modules/auth/auth.module";
+import { SharedModule } from "../shared/shared.module";
 
 import { HomeComponent } from "./pages/home/home.component";
 import { ProfileComponent } from "./pages/profile/profile.component";
 import { SplashScreenComponent } from "./components/splash-screen/splash-screen.component";
-import { SharedModule } from "../shared/shared.module";
 import { ImageUploadComponent } from "./components/image-upload/image-upload.component";
-import { ProfileImageComponent } from "./components/profile-image/profile-image.component";
 import { ShortcutComponent } from "./components/shortcut/shortcut.component";
 
 
@@ -15,7 +14,6 @@ import { ShortcutComponent } from "./components/shortcut/shortcut.component";
   declarations: [
     ImageUploadComponent,
     SplashScreenComponent,
-    ProfileImageComponent,
     ShortcutComponent,
     HomeComponent,
     ProfileComponent,
