d7eeee5e4c5eccccc484758a54b722ebb25e483a
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-10-08 21:51:38 -0300

    feat(core.shared): :children_crossing: add menu on layout

--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, inject } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { ShortcutsService } from "../../services/shortcuts.service";
+import { AuthService } from "src/app/core/modules/auth/services/auth.service";
+import { UserService } from "src/app/core/modules/auth/services/user.service";
 
 @Component({
   selector: "shared-layout",
@@ -8,8 +11,17 @@ import { ActivatedRoute } from "@angular/router";
 })
 export class LayoutComponent {
   private readonly route = inject(ActivatedRoute);
+  private readonly auth = inject(AuthService);
+  private readonly user = inject(UserService);
+  private readonly shortcutsSrv = inject(ShortcutsService);
+
   readonly mode: 1 | 2 | 3 | undefined = this.route.snapshot.data["mode"];
   @Input() loadingDM = false;
   @Input() loading = false;
   @Input() text?: string;
+
+  readonly $shortcuts = this.shortcutsSrv.$shortcuts;
+  readonly $user = this.user.$data;
+
+  logout(){ this.auth.logout(); }
 }
