0641a9ea0334f9c67f3369acb4916105ffdc92e0
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-10-19 18:44:40 -0300

    feat(routine): :recycle: refactor module and routingModule

--- src/app/modules/routine/routine.module.ts
+++ src/app/modules/routine/routine.module.ts
@@ -6,6 +6,11 @@ import { DetailDialogComponent } from "./components/detail-dialog/detail-dialog.
 import { MembersComponent } from "./components/members/members.component";
 import { DetailComponent } from "./pages/detail/detail.component";
 import { InformationComponent } from "./components/information/information.component";
+import { CustomerComponent } from "./components/customer/customer.component";
+import { ExercisesComponent } from "./components/exercises/exercises.component";
+import { ExerciseDialogComponent } from "./components/exercise-dialog/exercise-dialog.component";
+import { WeekDayComponent } from "./components/week-day/week-day.component";
+import { ExerciseComponent } from "./components/exercise/exercise.component";
 
 
 
@@ -14,7 +19,12 @@ import { InformationComponent } from "./components/information/information.compo
     DetailDialogComponent,
     MembersComponent,
     DetailComponent,
-    InformationComponent
+    InformationComponent,
+    CustomerComponent,
+    ExercisesComponent,
+    ExerciseDialogComponent,
+    WeekDayComponent,
+    ExerciseComponent
   ],
   imports: [
     RoutineRoutingModule,
--- src/app/modules/routine/routine.routing.ts
+++ src/app/modules/routine/routine.routing.ts
@@ -1,54 +1,12 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { CreateComponent } from "./pages/create/create.component";
 import { DetailComponent } from "./pages/detail/detail.component";
-import { FormGuard } from "src/app/shared/guards/form.guard";
 
 const routes: Routes = [
   {
-    path: "routines",
-    children: [
-      {
-        path: "create",
-        title: "Fitness - Trainer | Crear Rutina",
-        component: CreateComponent,
-        canDeactivate: [FormGuard],
-        data: {
-          layout: {
-            title: "Crear rutina",
-            close: true,
-            actions: [
-              { _id: "save", icon: "save", type: "submit" }
-            ]
-          }
-        }
-      },
-      {
-        path: ":_id",
-        title: "Fitness - Trainer | Actualizar Rutina",
-        component: DetailComponent,
-        data: {
-          layout: {
-            title: "Actualizar Rutina",
-            back: true
-          }
-        }
-      }
-    ]
-  },
-  {
-    path: "routine",
-    title: "Fitness - Trainer | Mi Rutina",
-    component: DetailComponent,
-    data: {
-      canComplete: true,
-      layout: {
-        title: "Mi Rutina",
-        back: true,
-        actions: []
-      }
-    }
-  },
+    path: ":_id",
+    component: DetailComponent
+  }
 ];
 
 @NgModule({
