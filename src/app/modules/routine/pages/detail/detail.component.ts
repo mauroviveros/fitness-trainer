17001196ebaf544a824ae9c136384efc681f92b6
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-10-20 16:03:43 -0300

    feat(routine): :wheelchair: only admins can change date by UI or weekDayComponent

--- src/app/modules/routine/pages/detail/detail.component.ts
+++ src/app/modules/routine/pages/detail/detail.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnDestroy, OnInit, inject } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
-import { Subscription, tap } from "rxjs";
+import { Subscription, map, switchMap, tap } from "rxjs";
 
 import { RoutineService } from "../../services/routine.service";
+import { UserService } from "src/app/core/modules/auth/services/user.service";
 
 import { Routine } from "src/app/shared/interfaces/routine";
 import { dayOfWeek } from "src/app/shared/interfaces/interfaces";
@@ -16,14 +17,17 @@ export class DetailComponent implements OnInit, OnDestroy {
   private readonly router = inject(Router);
   private readonly route = inject(ActivatedRoute);
   private readonly RoutineSrv = inject(RoutineService);
+  private readonly user = inject(UserService);
   private readonly subscriptions : Subscription[] = [];
   routine : Routine = {} as Routine;
   date : Date = new Date();
 
+  isAdmin = false;
   isLoading = true;
 
   ngOnInit(){
     this.subscriptions.push(this.initRoutine());
+    this.subscriptions.push(this.initUser());
   }
 
   ngOnDestroy(){
@@ -31,20 +35,35 @@ export class DetailComponent implements OnInit, OnDestroy {
   }
 
   private initRoutine() : Subscription {
-    return this.RoutineSrv.detail(this.route.snapshot.params["_id"]).pipe(
-      tap(() => this.isLoading = false)
+    return this.route.params.pipe(
+      tap(() => this.isLoading = false),
+      switchMap(params => params["_id"] ? this.RoutineSrv.detail(params["_id"]) : this.RoutineSrv.getOwn()),
+      map(response => Array.isArray(response) ? response : [response])
     ).subscribe({
       error: () => this.router.navigate(["/customers"]),
-      next: routine => this.routine = routine
+      next: routines => {
+        if(!this.route.snapshot.params["_id"]){
+          routines = routines.filter(routine => {
+            return this.date >= routine.dateIN && this.date <= routine.dateOUT;
+          });
+        }
+
+        if(!routines.length) this.router.navigate([""]);
+        else this.routine = routines[0];
+      }
     });
+  }
+
+  private initUser() : Subscription {
+    return this.user.$data.pipe(
+      map(user => user._admin)
+    ).subscribe(bool => this.isAdmin = bool);
   }
 
   dateFilter = (d: Date | null): boolean => {
     const cellDate = (d || new Date());
     if(!this.routine.dateIN || !this.routine.dateOUT) return false;
-    const isAfterIN = cellDate.getTime() >= this.routine.dateIN.getTime();
-    const isBeforeOUT = cellDate.getTime() <= this.routine.dateOUT.getTime();
     const isIncludeInDays = this.routine.daysOfWeek.includes(cellDate.getDay() as dayOfWeek);
-    return isAfterIN && isBeforeOUT && isIncludeInDays;
+    return cellDate >= this.routine.dateIN && cellDate <= this.routine.dateOUT && isIncludeInDays;
   };
 }
