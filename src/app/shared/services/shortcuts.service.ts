14d92ed53771ffc83c1858a7835a834309f97990
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-10-21 14:36:10 -0300

    feat(core.shared): :zap: improve performance on get data for shortcuts

--- src/app/shared/services/shortcuts.service.ts
+++ src/app/shared/services/shortcuts.service.ts
@@ -1,28 +1,41 @@
 import { Injectable, inject } from "@angular/core";
-import { Shortcut } from "../interfaces/shortcut";
+import { BehaviorSubject, combineLatest, filter, map } from "rxjs";
+
 import { UserService } from "src/app/core/modules/auth/services/user.service";
-import { UserDoc } from "../interfaces/user";
-import { map } from "rxjs";
+import { RoutineService } from "src/app/modules/routine/services/routine.service";
+
+import { Shortcut } from "../interfaces/shortcut";
+import { Routine } from "../interfaces/routine";
 
 @Injectable({
   providedIn: "root"
 })
 export class ShortcutsService {
   private readonly user = inject(UserService);
+  private readonly routine = inject(RoutineService);
 
+  private readonly subject = new BehaviorSubject<Shortcut[] | undefined>(undefined);
   private readonly SHORTCUTS: Shortcut[] = [
     { _id: "home", icon: "home", label: "Inicio", link: "/" },
-    { _id: "exercises", icon: "fitness_center", label: "ejercicios", link: "/exercises", admin: true },
+    { _id: "routine", icon: "accessibility", label: "Mi rutina", link: "/routine" },
     { _id: "customers", icon: "people", label: "clientes", link: "/customers", admin: true },
+    { _id: "exercises", icon: "fitness_center", label: "ejercicios", link: "/exercises", admin: true },
     { _id: "profile", icon: "manage_accounts", label: "mis datos", link: "/profile" },
     { _id: "logout", icon: "logout", label: "cerrar sesión" }
   ];
 
-  get shortcuts(){
-    return this.SHORTCUTS;
-  }
-  $shortcuts = this.user.$data.pipe(
-    map(user => user._admin),
-    map(isAdmin => this.SHORTCUTS.filter(shortcut => shortcut.admin ? isAdmin : true))
+  readonly $shortcuts = this.subject.asObservable().pipe(
+    filter(shortcuts => !!shortcuts),
+    map(shortcuts => shortcuts as Shortcut[])
   );
+
+  constructor(){
+    combineLatest([this.user.$data, this.routine.getOwn()]).pipe(
+      map(([user, routines]) => [user._admin, routines.filter(routine => new Date() >= routine.dateIN && new Date() <= routine.dateOUT)] as [boolean, Routine[]]),
+      map(([isAdmin, routines]) => this.SHORTCUTS.filter(shortcut => shortcut._id === "routine" && !routines.length ? false : shortcut.admin ? isAdmin : true))
+    ).subscribe(shortcuts => {
+      this.subject.next(shortcuts);
+    });
+  }
+
 }
