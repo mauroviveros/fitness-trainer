950c4ff74fcc5c098478bc5b8d1280059cce3d81
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2024-02-14 04:57:40 -0300

    refactor(auth): :recycle: use wrapperComponent on login and register page

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SharedModule } from '../../../shared/shared.module';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { WrapperComponent } from '../../components/wrapper/wrapper.component';
 
+interface FormType {
+  email: FormControl<string | null>;
+  password: FormControl<string | null>;
+}
 @Component({
   selector: 'auth-login',
   standalone: true,
-  imports: [],
+  imports: [SharedModule, WrapperComponent],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  private readonly fb = inject(FormBuilder);
+
+  readonly form: FormGroup<FormType> = this.fb.group<FormType>({
+    email: this.fb.control(null, [Validators.required, Validators.email]),
+    password: this.fb.control(null, [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
+
+  submit() {
+    console.log(this.form.value);
+  }
+}
--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SharedModule } from '../../../shared/shared.module';
+import { WrapperComponent } from '../../components/wrapper/wrapper.component';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+interface FormType {
+  email: FormControl<string | null>;
+  password: FormControl<string | null>;
+  password_confirm: FormControl<string | null>;
+}
 
 @Component({
   selector: 'auth-register',
   standalone: true,
-  imports: [],
+  imports: [SharedModule, WrapperComponent],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  private readonly fb = inject(FormBuilder);
+
+  readonly form: FormGroup<FormType> = this.fb.group<FormType>({
+    email: this.fb.control(null, [Validators.required, Validators.email]),
+    password: this.fb.control(null, [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+    password_confirm: this.fb.control(null, [Validators.required]),
+  });
+
+  submit() {
+    console.log(this.form.value);
+  }
+}
