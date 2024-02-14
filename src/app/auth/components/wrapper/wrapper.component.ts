7d52eaeb6c219d52424fbac7e9638b7a45e349e7
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2024-02-14 04:25:28 -0300

    feat(auth): :sparkles: add wrapperComponent

---
+++ src/app/auth/components/wrapper/wrapper.component.ts
@@ -0,0 +1,26 @@
+import { Component, inject } from '@angular/core';
+import { SharedModule } from '../../../shared/shared.module';
+import { ActivatedRoute } from '@angular/router';
+
+interface wrapper {
+  title: string;
+  footer: {
+    text: string;
+    link: {
+      label: string;
+      route: string | string[] | null | undefined;
+    };
+  };
+}
+
+@Component({
+  selector: 'auth-wrapper',
+  standalone: true,
+  imports: [SharedModule],
+  templateUrl: './wrapper.component.html',
+  styleUrl: './wrapper.component.scss',
+})
+export class WrapperComponent {
+  private readonly route = inject(ActivatedRoute);
+  readonly wrapper: wrapper = this.route.snapshot.data['wrapper'];
+}
