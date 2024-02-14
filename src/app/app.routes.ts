6f196744dfbe0e6e8ac72ac0147956a7e42b4dff
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2024-02-14 04:54:30 -0300

    feat: :wheelchair: add data for wrapperComponent and title in routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,36 @@ import { RegisterComponent } from './auth/pages/register/register.component';
 export const routes: Routes = [
   {
     path: 'login',
+    title: 'Fitness Trainer | Identifícate',
     component: LoginComponent,
+    data: {
+      wrapper: {
+        title: 'Iniciar sesión',
+        footer: {
+          text: '¿No tienes cuenta?',
+          link: {
+            route: '/register',
+            label: 'Regístrate aquí',
+          },
+        },
+      },
+    },
   },
   {
     path: 'register',
+    title: 'Fitness Trainer | Crea tu cuenta',
     component: RegisterComponent,
+    data: {
+      wrapper: {
+        title: 'Registro',
+        footer: {
+          text: '¿Ya tienes una cuenta?',
+          link: {
+            route: '/login',
+            label: 'Inicia sesión aquí',
+          },
+        },
+      },
+    },
   },
 ];
