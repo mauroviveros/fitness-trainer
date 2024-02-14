fba52a5c04da00abd5831f5cf8d2320899902bf9
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2024-02-14 05:31:26 -0300

    feat(shared): :goal_net: add errorPipe for forms

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { MaterialModule } from './modules/material/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { ErrorPipe } from './pipes/error.pipe';
 
 @NgModule({
-  exports: [MaterialModule, ReactiveFormsModule, RouterModule],
+  imports: [ErrorPipe],
+  exports: [MaterialModule, ReactiveFormsModule, RouterModule, ErrorPipe],
 })
 export class SharedModule {}
