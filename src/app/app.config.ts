a7e85f5c03c29f7dd0e63e0e5bc180b11d7d3bca
Mauro Daniel viveros <maurod.viveros@gmail.com>
2024-12-09 00:31:09 -0300

    build: 📦 setup firebase w/ angular

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,14 +3,28 @@ import {
   provideExperimentalZonelessChangeDetection
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import {
+  getAnalytics,
+  provideAnalytics,
+  ScreenTrackingService,
+  UserTrackingService
+} from '@angular/fire/analytics';
+
+import { FIREBASE } from 'environments/firebase';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
     provideRouter(routes),
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp(FIREBASE)),
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics()),
+    ScreenTrackingService,
+    UserTrackingService
   ]
 };
