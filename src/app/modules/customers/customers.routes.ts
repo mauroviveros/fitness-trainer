d4f809052c886cbc78910bbd7c941f836f3cde27
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-02-19 01:24:21 -0300

    feat(customers): 🚧 init customers

---
+++ src/app/modules/customers/customers.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { CustomersComponent } from './customers.component';
+
+export const routes: Routes = [{ path: '', component: CustomersComponent }];
