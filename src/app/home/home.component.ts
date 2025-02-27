e1f61b1f65d19647e05c89e55d13b4cde8ad1345
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-02-27 00:40:33 -0300

    refactor: ♻️ refactor homeComponent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,30 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { AuthService } from '@auth/auth.service';
-import { ShortcutsComponent } from './shortcuts/shortcuts.component';
+import { RouterLink } from '@angular/router';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+
+import { NavigationService } from '@shared/services/navigation.service';
+
+const MATERIAL_MODULES = [
+  MatButtonModule,
+  MatCardModule,
+  MatGridListModule,
+  MatIconModule
+];
 
 @Component({
   selector: 'home-wrapper',
-  imports: [ShortcutsComponent],
+  imports: [RouterLink, MATERIAL_MODULES],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent {
-  private readonly auth = inject(AuthService);
-
-  logout() {
-    return this.auth.signOut();
-  }
+  private readonly navigation = inject(NavigationService);
+  readonly shortcuts = this.navigation.routes.filter(
+    ({ shortcut }) => shortcut
+  );
 }
